f0f1c9bbec6774e801ec177f46fd64020b3fb099
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2025-01-07 11:45:47 +1100

    Refactor fetchNewRandomPoem into one action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,12 +1,6 @@
 "use server"
 import { Poem } from "./types";
 
-// export const getRandomPoem = async (): Promise<Poem[]> => {
-//     const data = await fetch("https://poetrydb.org/random");
-//     const newPoemArray = data.json();
-//     return newPoemArray;
-// };
-
 export const fetchNewRandomPoem = async (): Promise<Poem> => {
     const data = await fetch("https://poetrydb.org/random");
     const newPoemArray: Poem[] = await data.json();
