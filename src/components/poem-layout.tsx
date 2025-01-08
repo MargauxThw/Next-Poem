63ca6ebc0e614ecb2fde85e69685c5bd3dca897a
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2025-01-08 18:39:18 +1100

    Prettier formatting changes

--- src/components/poem-layout.tsx
+++ src/components/poem-layout.tsx
@@ -7,34 +7,33 @@ import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 
 export default function PoemLayout({ child }: { child: Poem }) {
-  const [poem, setPoem] = useState<Poem>(child);
+	const [poem, setPoem] = useState<Poem>(child);
 
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-    setPoem(child);
-  }, [child]);
+	useEffect(() => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+		setPoem(child);
+	}, [child]);
 
-  return (
-    <>
-      <div className="flex flex-col items-start sm:items-start gap-4">
-        <h2 className="decoration-black font-bold text-xl">{poem.title}</h2>
-        <h3 className="italic text-lg">{poem.author}</h3>
-      </div>
+	return (
+		<>
+			<div className="flex flex-col items-start sm:items-start gap-4">
+				<h2 className="decoration-black font-bold text-xl">
+					{poem.title}
+				</h2>
+				<h3 className="italic text-lg">{poem.author}</h3>
+			</div>
 
-      <Separator />
-      <section>
-        {poem.lines.map((str, index) => (
-          <Markdown
-            rehypePlugins={[rehypeRaw]}
-            key={index}
-          >
-            {`${str.trimStart()}<br/>`}
-          </Markdown>
-        ))}
-      </section>
-    </>
-  );
+			<Separator />
+			<section>
+				{poem.lines.map((str, index) => (
+					<Markdown rehypePlugins={[rehypeRaw]} key={index}>
+						{`${str.trimStart()}<br/>`}
+					</Markdown>
+				))}
+			</section>
+		</>
+	);
 }
