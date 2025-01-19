ad6fc1382a8dc457008583b606cc72daf200c819
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2025-01-19 15:28:58 +1100

    Disable likes for unauthenticated users

--- src/components/like-button.tsx
+++ src/components/like-button.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { Heart } from "lucide-react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export default function LikeButton({ isAnimating }: { isAnimating: boolean }) {
 	const [heart, setHeart] = useState<boolean>(false);
@@ -12,17 +14,45 @@ export default function LikeButton({ isAnimating }: { isAnimating: boolean }) {
 	};
 
 	return (
-		<Button
-			variant="outline"
-			onClick={handleHeartClick}
-			size="icon"
-			disabled={isAnimating}
-		>
-      <span className="sr-only">{`${heart ? "Unlike poem" : "Like poem"}`}</span>
-			<Heart
-				fill={heart ? "red" : "white"}
-				stroke={heart ? "bg-inherit" : "black"}
-			/>
-		</Button>
+		<>
+			<SignedIn>
+				<Button
+					variant="outline"
+					onClick={handleHeartClick}
+					size="icon"
+					disabled={isAnimating}
+				>
+					<span className="sr-only">{`${
+						heart ? "Unlike poem" : "Like poem"
+					}`}</span>
+					<Heart
+						fill={heart ? "red" : "white"}
+						stroke={heart ? "bg-inherit" : "black"}
+					/>
+				</Button>
+			</SignedIn>
+			<SignedOut>
+				<Popover>
+					<PopoverTrigger asChild>
+						<Button
+							variant="outline"
+							size="icon"
+							disabled={isAnimating}
+						>
+							<span className="sr-only">{`${
+								heart ? "Unlike poem" : "Like poem"
+							}`}</span>
+							<Heart
+								fill={"white"}
+								stroke={"black"}
+							/>
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent>
+						<p>Log in or sign up to save poems</p>
+					</PopoverContent>
+				</Popover>
+			</SignedOut>
+		</>
 	);
 }
