f1ecd5cb412802f0e91234a1d17e7063117b78ec
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2025-01-19 11:12:58 +1100

    Add random poem button to Browse poem action bar

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -111,169 +111,182 @@ export default function Page() {
 		setCurrentPoemIndex(sortedPoems.indexOf(poem));
 	};
 
-	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-full p-4 pb-8 gap-4 sm:p-20 animate-blur-in">
-			{browseMode ? (
-				<main
-					className={`flex flex-col gap-8 row-start-2 items-start sm:items-start w-full max-w-lg h-fit`}
-				>
-					<div className="flex flex-col items-start sm:items-start gap-4 w-full">
-						<h2 className="decoration-black font-bold text-xl">
-							Browse poems
-						</h2>
-						<Separator />
-						<div className="flex flex-row gap-2 w-full flex-wrap mb-0">
-							<FilterButton initiateFetch={updatePoemList} />
-							<Select
-								value={sortMode}
-								onValueChange={(value) =>
-									setSortMode(
-										Object.values(sortingOption).find(
-											(v) => v === value
-										) || sortMode
-									)
-								}
-							>
-								<SelectTrigger className="w-min justify-start gap-2">
-									<span className="text-muted-foreground">
-										Sort:
-									</span>
-									<SelectValue />
-								</SelectTrigger>
-								<SelectContent>
-									{Object.values(sortingOption).map(
-										(option, index) => (
-											<SelectItem
-												key={index}
-												value={option}
-											>
-												{option}
-											</SelectItem>
-										)
-									)}
-								</SelectContent>
-							</Select>
-						</div>
-						<Separator />
-					</div>
-					{hasError ? (
-						<p>{errorMessage}</p>
-					) : poems && poems.length > 0 ? (
-						<div
-							className={`flex flex-col gap-4 w-full -mt-2 animate-blur-in`}
-						>
-							<Badge
-								variant={"outline"}
-								className="border-0 text-muted-foreground justify-end px-0 py-0 -mt-1 font-light"
-							>
-								{`Poems ${
-									currentPage === 1
-										? 1
-										: currentPage * 10 - 10 + 1
-								}－${
-									currentPage * 10 >= poems.length
-										? poems.length
-										: currentPage * 10
-								} | ${sortedPoems.length} results`}
-							</Badge>
+	const getRandomPoemIndex = () => {
+		let randomIndex = Math.floor(Math.random() * sortedPoems.length);
+		if (randomIndex === currentPoemIndex) {
+			randomIndex += 1;
+			if (randomIndex >= sortedPoems.length) {
+				randomIndex = 0;
+			}
+		}
+		return randomIndex;
+	};
 
-							{sortedPoems
-								.slice(
-									currentPage === 1
-										? 0
-										: currentPage * 10 - 10,
-									currentPage * 10 >= poems.length
-										? poems.length
-										: currentPage * 10
-								)
-								.map((poem, index) => {
+		return (
+			<div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-full p-4 pb-8 gap-4 sm:p-20 animate-blur-in">
+				{browseMode ? (
+					<main
+						className={`flex flex-col gap-8 row-start-2 items-start sm:items-start w-full max-w-lg h-fit`}
+					>
+						<div className="flex flex-col items-start sm:items-start gap-4 w-full">
+							<h2 className="decoration-black font-bold text-xl">
+								Browse poems
+							</h2>
+							<Separator />
+							<div className="flex flex-row gap-2 w-full flex-wrap mb-0">
+								<FilterButton initiateFetch={updatePoemList} />
+								<Select
+									value={sortMode}
+									onValueChange={(value) =>
+										setSortMode(
+											Object.values(sortingOption).find(
+												(v) => v === value
+											) || sortMode
+										)
+									}
+								>
+									<SelectTrigger className="w-min justify-start gap-2">
+										<span className="text-muted-foreground">
+											Sort:
+										</span>
+										<SelectValue />
+									</SelectTrigger>
+									<SelectContent>
+										{Object.values(sortingOption).map(
+											(option, index) => (
+												<SelectItem
+													key={index}
+													value={option}
+												>
+													{option}
+												</SelectItem>
+											)
+										)}
+									</SelectContent>
+								</Select>
+							</div>
+							<Separator />
+						</div>
+						{hasError ? (
+							<p>{errorMessage}</p>
+						) : poems && poems.length > 0 ? (
+							<div
+								className={`flex flex-col gap-4 w-full -mt-2 animate-blur-in`}
+							>
+								<Badge
+									variant={"outline"}
+									className="border-0 text-muted-foreground justify-end px-0 py-0 -mt-1 font-light"
+								>
+									{`Poems ${
+										currentPage === 1
+											? 1
+											: currentPage * 10 - 10 + 1
+									}－${
+										currentPage * 10 >= poems.length
+											? poems.length
+											: currentPage * 10
+									} | ${sortedPoems.length} results`}
+								</Badge>
+
+								{sortedPoems
+									.slice(
+										currentPage === 1
+											? 0
+											: currentPage * 10 - 10,
+										currentPage * 10 >= poems.length
+											? poems.length
+											: currentPage * 10
+									)
+									.map((poem, index) => {
+										return (
+											<PoemCard
+												key={index}
+												poem={poem}
+												openPoem={openPoem}
+											/>
+										);
+									})}
+							</div>
+						) : (
+							<div
+								className={`flex flex-col gap-4 w-full -mt-2 animate-pulse`}
+							>
+								<Badge
+									variant={"outline"}
+									className="border-0 text-right text-muted-foreground justify-end px-0"
+								>
+									Loading
+								</Badge>
+								{samplePoemList.map((poem, index) => {
 									return (
 										<PoemCard
 											key={index}
 											poem={poem}
-											openPoem={openPoem}
+											openPoem={() => {}}
 										/>
 									);
 								})}
-						</div>
-					) : (
-						<div
-							className={`flex flex-col gap-4 w-full -mt-2 animate-pulse`}
-						>
-							<Badge
-								variant={"outline"}
-								className="border-0 text-right text-muted-foreground justify-end px-0"
+							</div>
+						)}
+						{totalPages !== 1 && !isNew && !hasError ? (
+							<>
+								<Separator className="animate-blur-in" />
+								<BrowsePagination
+									currentPage={currentPage}
+									totalPages={totalPages}
+									setCurrentPage={changePage}
+								/>
+							</>
+						) : (
+							<></>
+						)}
+					</main>
+				) : (
+					<main
+						className={`flex flex-col gap-8 row-start-0 items-start sm:items-start w-full max-w-lg h-fit ${
+							isNew ? "animate-blur-in" : ""
+						}`}
+					>
+						<div className="flex flex-row items-center justify-between w-full">
+							<Button
+								variant={"link"}
+								className="px-0"
+								onClick={() => setBrowseMode(true)}
 							>
-								Loading
-							</Badge>
-							{samplePoemList.map((poem, index) => {
-								return (
-									<PoemCard
-										key={index}
-										poem={poem}
-										openPoem={() => {}}
-									/>
-								);
-							})}
+								<MoveLeft />
+								Back to browse
+							</Button>
+							<Badge variant={"secondary"} className="px-1">{`${
+								currentPoemIndex + 1
+							} of ${sortedPoems.length}`}</Badge>
 						</div>
-					)}
-					{totalPages !== 1 && !isNew && !hasError ? (
-						<>
-							<Separator className="animate-blur-in" />
-							<BrowsePagination
-								currentPage={currentPage}
-								totalPages={totalPages}
-								setCurrentPage={changePage}
-							/>
-						</>
-					) : (
-						<></>
-					)}
-				</main>
-			) : (
-				<main
-					className={`flex flex-col gap-8 row-start-0 items-start sm:items-start w-full max-w-lg h-fit ${
-						isNew ? "animate-blur-in" : ""
-					}`}
-				>
-					<div className="flex flex-row items-center justify-between w-full">
-						<Button
-							variant={"link"}
-							className="px-0"
-							onClick={() => setBrowseMode(true)}
-						>
-							<MoveLeft />
-							Back to browse
-						</Button>
-						<Badge variant={"secondary"} className="px-1">{`${
-							currentPoemIndex + 1
-						} of ${sortedPoems.length}`}</Badge>
-					</div>
-					<PoemLayout child={sortedPoems[currentPoemIndex]} />
-					<Separator />
-					<BrowseActionBar
-						next={
-							currentPoemIndex + 1 < sortedPoems.length
-								? () =>
-										setCurrentPoemIndex(
-											currentPoemIndex + 1
-										)
-								: () => setCurrentPoemIndex(0)
-						}
-						prev={
-							currentPoemIndex - 1 >= 0
-								? () =>
-										setCurrentPoemIndex(
-											currentPoemIndex - 1
-										)
-								: () =>
-										setCurrentPoemIndex(
-											sortedPoems.length - 1
-										)
-						}
-					/>
-				</main>
-			)}
-		</div>
-	);
-}
+						<PoemLayout child={sortedPoems[currentPoemIndex]} />
+						<Separator />
+						<BrowseActionBar
+							next={
+								currentPoemIndex + 1 < sortedPoems.length
+									? () =>
+											setCurrentPoemIndex(
+												currentPoemIndex + 1
+											)
+									: () => setCurrentPoemIndex(0)
+							}
+							prev={
+								currentPoemIndex - 1 >= 0
+									? () =>
+											setCurrentPoemIndex(
+												currentPoemIndex - 1
+											)
+									: () =>
+											setCurrentPoemIndex(
+												sortedPoems.length - 1
+											)
+							}
+							random={() => setCurrentPoemIndex(getRandomPoemIndex())}
+							numPoems={sortedPoems.length}
+						/>
+					</main>
+				)}
+			</div>
+		);
+	};
--- src/components/browse-action-bar.tsx
+++ src/components/browse-action-bar.tsx
@@ -1,27 +1,54 @@
 "use client";
 
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, Shuffle } from "lucide-react";
 import LikeButton from "./like-button";
 import { Button } from "./ui/button";
 
 export default function BrowseActionBar({
 	next,
 	prev,
+	random,
+	numPoems,
 }: {
 	next: () => void;
 	prev: () => void;
+	random: () => void;
+	numPoems: number;
 }) {
 	return (
 		<div className="flex gap-2">
 			<LikeButton isAnimating={false} />
-			<Button variant="outline" onClick={prev} className="pl-2">
+			{numPoems > 1 ? (
+				<>
+					<Button variant="outline" onClick={random} size={"icon"}>
+						<span className="sr-only">Next random poem</span>
+						<Shuffle />
+					</Button>
+					<Button variant="outline" onClick={prev} size="icon">
+						<span className="sr-only">
+							Previous poem in browse order
+						</span>
+						<ChevronLeft />
+					</Button>
+					<Button variant="outline" onClick={next} size="icon">
+						<span className="sr-only">
+							Next poem in browse order
+						</span>
+						<ChevronRight />
+					</Button>
+				</>
+			) : (
+				<> </>
+			)}
+
+			{/* <Button variant="outline" onClick={prev} className="pl-2">
 				<ChevronLeft />
 				Previous poem
 			</Button>
 			<Button variant="outline" onClick={next} className="pr-2">
 				Next poem
 				<ChevronRight />
-			</Button>
+			</Button> */}
 		</div>
 	);
 }
